0f9858d2d741fcc792ddb24fb0c8bd3bcd11eab8
Gnarll <vadimfedorovich22@mail.ru>
2022-11-25 04:25:58 +0300

    connect to mongodb and make user model

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Response successed!");
-});
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.m9ustyg.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB OK");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.post("/auth/register", (req, res) => {});
 
 app.listen("4444", (err) => {
   if (err) return console.log(err);
